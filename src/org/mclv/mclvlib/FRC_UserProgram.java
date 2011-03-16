06d19bd7ff15380924f4a0ad984f00b1f47a1b98
Alex Roman <computeralex@gmail.com>
2011-03-15 23:43:19 -0400

    netbeans project

---
+++ src/org/mclv/mclvlib/FRC_UserProgram.java
@@ -0,0 +1,28 @@
+package org.mclv.mclvlib;
+
+/*
+ * StartApplication.java
+ *
+ */
+import javax.microedition.midlet.MIDlet; //DAMMIT MICRO EDITION, DAMN YOU!
+import javax.microedition.midlet.MIDletStateChangeException;
+/**
+ * The startApp method of this class is called by the VM to start the
+ * application.
+ * 
+ * The manifest specifies this class as MIDlet-1, which means it will
+ * be selected for execution.
+ */
+public class FRC_UserProgram extends MIDlet {
+
+    protected void startApp() throws MIDletStateChangeException {
+        System.out.println("Hello World");
+    }
+
+    protected void pauseApp() {
+    }
+
+    protected void destroyApp(boolean unconditional) throws MIDletStateChangeException {
+    }
+    
+}
